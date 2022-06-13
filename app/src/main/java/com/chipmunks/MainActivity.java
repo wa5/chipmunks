60c97ff1986ad2c62de61cdf6a13bfcc8f983ce7
Kqqiihs <104705270+Kqqiihs@users.noreply.github.com>
2022-06-13 20:26:36 +0530

    Initial commit

---
+++ app/src/main/java/com/chipmunks/MainActivity.java
@@ -0,0 +1,14 @@
+package com.chipmunks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
